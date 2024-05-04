dca40fb6d09075260a8e437338ab9a21002ed107
amir-memarian <memarian.email@gmail.com>
2024-05-04 18:40:06 +0330

    sphere surface area - exe0003

---
+++ exercise0003.py
@@ -0,0 +1,8 @@
+pi = 22 / 7
+radius = float(input('Radius of sphere: '))
+
+surArea = 4 * pi * (radius ** 2)
+volume = (4/3) * (pi * (radius ** 3))
+
+print(f'surface Area is: {surArea}')
+print(f'volume is: {volume}')
